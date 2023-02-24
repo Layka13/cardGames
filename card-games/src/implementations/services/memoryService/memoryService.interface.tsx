591e8ad8f7b1e11d55fde88376b208da0ef0b2c9
Luka Mulder <luka.mulder@nn.nl>
2023-02-24 16:05:57 +0100

    start memory

---
+++ card-games/src/implementations/services/memoryService/memoryService.interface.tsx
@@ -0,0 +1,7 @@
+import { Icard } from "../../../interfaces/Icards";
+
+export interface MemoryCard {
+  code: string;
+  image: string;
+  matched: boolean;
+}
---
+++ card-games/src/implementations/services/memoryService/memoryService.tsx
@@ -0,0 +1,18 @@
+import { MemoryCard } from "./memoryService.interface";
+import { DeckRepository } from "../../repository/deckRepository";
+import { Icard, Ideck } from "../../../interfaces/Icards";
+
+export class memoryService {
+  public async getNewGame(): Promise<MemoryCard[]> {
+    const deckRepository = new DeckRepository();
+    const deck: Ideck = await deckRepository.getNewDeck(2);
+    const cards: Icard[] = await deckRepository.drawCard(deck.deck_id, 104);
+    return cards.map((card) => {
+      return { code: card.code, image: card.image, matched: false };
+    });
+  }
+
+  public isSameCard(one: MemoryCard, two: MemoryCard): boolean {
+    return one.code === two.code;
+  }
+}
